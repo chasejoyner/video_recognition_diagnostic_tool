4ee1bdd30143572f2c30debfb32c03093870b2b0
Chase Joyner <chasej@clemson.edu>
2025-04-13 16:49:27 -0400

    Working version which allows to record, label, and cycle back

--- main.py
+++ main.py
@@ -11,12 +11,8 @@ from tkinter import ttk, messagebox
 from PIL import Image, ImageTk
 
 # Initalize logger
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-console_handler = logging.StreamHandler()
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-console_handler.setFormatter(formatter)
-logger.addHandler(console_handler)
-logger.setLevel(logging.INFO)
 
 
 class PoseRecorderApp:
@@ -47,203 +43,192 @@ class PoseRecorderApp:
         self.fps = fps
 
         # Initialize pose and recording objects
-        self.mp_pose = mp.solutions.pose
-        self.pose = self.mp_pose.Pose()
-        self.prev_landmarks = {}
+        self.mpPose = mp.solutions.pose
+        self.pose = self.mpPose.Pose()
+        self.prevLandmarks = {}
         self.cap = cv2.VideoCapture(0)
-
         self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
         self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
-        self.frame_width = int(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-        self.frame_height = int(self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-
+        self.frameWidth = int(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        self.frameHeight = int(self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
         self.fourcc = cv2.VideoWriter_fourcc(*'XVID')
-        self.used_landmark_indices = set()
+        self.usedLandmarkIndices = set()
         for connection in self.POSE_CONNECTIONS:
-            self.used_landmark_indices.update(connection)
+            self.usedLandmarkIndices.update(connection)
 
         # Nuance parameters
         self.recording = False
         self.out = None
-        self.start_time = 0
-        self.last_filename = None
+        self.startTime = 0
+        self.lastFilename = None
 
         # GUI
-        self.root = tk.Tk()
-        self.root.title("Swing Recorder")
-        self.root.configure(bg="#1e1e1e")
-        self.style = ttk.Style(self.root)
-        self.style.theme_use('clam')  # 'clam', 'alt', or 'default'
+        self.gui = tk.Tk()
+        self.gui.minsize(800, 600)
+        self.gui.title('Swing Recorder')
+        self.gui.configure(bg='#1e1e1e')
+        self.guiStyle = ttk.Style(self.gui)
+        self.guiStyle.theme_use('clam')
 
-        # Configure grid layout: 90% for video, 10% for buttons
-        self.root.grid_rowconfigure(0, weight=8)  # 90% of window for video
-        self.root.grid_rowconfigure(1, weight=2)  # 10% of window for buttons
-        self.root.grid_columnconfigure(0, weight=1)  # Full width for both video and buttons
+        # Create video frame in top row
+        self.videoFrame = tk.Frame(self.gui, bg='#1e1e1e')
+        self.videoFrame.pack(side='top', fill='both', expand=True)
+        self.videoFrame.pack_propagate(False)
+        self.videoSection = tk.Label(self.videoFrame)
+        self.videoSection.pack(padx=10, pady=10, fill='both', expand=True)
 
-        # Video label (will occupy 90% of the window)
-        self.video_label = tk.Label(self.root)
-        self.video_label.grid(row=0, column=0, padx=10, pady=10, sticky="nsew")  # Fill available space
+        # Create text frame in middle row
+        self.textFrame = tk.Frame(self.gui, bg='#1e1e1e')
+        self.textFrameText = tk.Label(self.textFrame, text='Select an option:', font=('Arial', 14), fg='white', bg='#1e1e1e')
+        self.textFrame.pack(fill='x', pady=0)
+        self.textFrameText.pack()
 
-        self.countdown_label = tk.Label(self.root, text="", font=("Arial", 16), bg="#1e1e1e", fg="white")
-        self.countdown_label.grid(row=1, column=0, pady=10)
-
-        # Buttons (placed in the 10% bottom area)
-        self.btn_record = tk.Button(self.root, text="Record 5 Seconds", command=self.record_video, font=("Arial", 14),
-                                    width=20)
-        self.btn_record.grid(row=1, column=0, pady=20)
-
-        self.btn_good = tk.Button(self.root, text="Good", command=self.save_good, font=("Arial", 14), width=20)
-        self.btn_bad = tk.Button(self.root, text="Bad", command=self.save_bad, font=("Arial", 14), width=20)
+        # Create button frame in bottom row
+        self.buttonFrame = tk.Frame(self.gui, height=100, bg='#1e1e1e')
+        self.buttonFrame.pack(side='bottom', fill='x', pady=10)
+        self.buttonFrame.pack_propagate(False)
+        self.btnRecord = tk.Button(self.buttonFrame, text='Record 5 Seconds', command=self.recordVideo, font=('Arial', 14), width=20)
+        self.btnRecord.pack(side='left', expand=True, padx=10)
+        self.btnGood = tk.Button(self.buttonFrame, text='Good', command=self.saveGood, font=('Arial', 14), width=20)
+        self.btnBad = tk.Button(self.buttonFrame, text='Bad', command=self.saveBad, font=('Arial', 14), width=20)
+        self.countdownText = tk.Label(self.buttonFrame, font=('Arial', 14), fg='white', bg='#1e1e1e')
 
         # Start threading and program
+        self.videoLoop()
+        self.gui.mainloop()
 
-        self.video_loop()
-        self.root.mainloop()
-
-    def record_video(self):
+    def recordVideo(self):
         """
         Function call to start recording a predetermined long video and save
         """
         if self.recording:
             return
+        self.textFrameText.config(text='Recording:')
         self.recording = True
-        self.start_time = time.time()
-        timestamp = time.strftime("%Y%m%d-%H%M%S")
-        self.last_filename = f"pose_capture_{timestamp}.avi"
+        self.startTime = time.time()
+        timestamp = time.strftime('%Y%m%d-%H%M%S')
+        self.lastFilename = f'pose_capture_{timestamp}.avi'
+        self.out = cv2.VideoWriter(self.lastFilename, self.fourcc, self.fps, (self.frameWidth, self.frameHeight))
+        logger.info('Started recording...')
+        self.btnRecord.pack_forget()
+        self.btnGood.pack_forget()
+        self.btnBad.pack_forget()
+        self.countdownText.pack(side='top', expand=True, padx=10)
+        self.updateCountdown()
 
-        self.out = cv2.VideoWriter(self.last_filename, self.fourcc, self.fps, (self.frame_width, self.frame_height))
-
-        logger.info("Started recording...")
-
-        # Update countdown
-
-        self.update_countdown()
-
-    def update_countdown(self):
+    def updateCountdown(self):
         """
         Function to update the countdown in the UI during recording
         """
         if self.recording:
-            elapsed_time = time.time() - self.start_time
-            remaining_time = max(0, self.recordingLength - int(elapsed_time))
-            self.countdown_label.config(text=f"Recording: {remaining_time} sec")
+            elapsedTime = time.time() - self.startTime
+            remainingTime = max(0, self.recordingLength - int(elapsedTime))
+            self.countdownText.config(text=f'{remainingTime} sec')
 
-            if remaining_time > 0:
+            if remainingTime > 0:
+                self.gui.after(1000, self.updateCountdown)
 
-                self.root.after(1000, self.update_countdown)  # Update every second
-            else:
-
-                self.recording = False
-
-                self.out.release()
-                self.out = None
-                logger.info("Recording completed.")
-                self.countdown_label.config(text="Recording done")  # Change text when done
-                # Hide the "Record 5 Seconds" button and show the "Good" and "Bad" buttons
-                self.btn_record.grid_forget()
-
-                self.btn_good.place(relx=0.4, rely=1, anchor="center")
-                self.btn_bad.place(relx=0.6, rely=1, anchor="center")
-
-    def video_loop(self):
+    def videoLoop(self):
         ret, frame = self.cap.read()
         if not ret:
-            self.root.after(10, self.video_loop)
+            self.gui.after(10, self.videoLoop)
             return
 
-        img_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        results = self.pose.process(img_rgb)
+        imgRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        results = self.pose.process(imgRGB)
 
         # Landmarks detected, draw nodes and connections
         if results.pose_landmarks:
-
-            smoothed_points = {}
-
-            for idx in self.used_landmark_indices:
+            smoothedPoints = {}
+            for idx in self.usedLandmarkIndices:
                 landmark = results.pose_landmarks.landmark[idx]
                 h, w, _ = frame.shape
-                raw_x, raw_y = landmark.x * w, landmark.y * h
-                cx, cy = self.smooth_landmark(idx, (raw_x, raw_y))
-                smoothed_points[idx] = (cx, cy)
+                rawX, rawY = landmark.x * w, landmark.y * h
+                cx, cy = self.smoothLandmark(idx, (rawX, rawY))
+                smoothedPoints[idx] = (cx, cy)
                 cv2.circle(frame, (cx, cy), 5, (0, 255, 0), -1)
 
             for connection in self.POSE_CONNECTIONS:
-                start_idx, end_idx = connection
-                if start_idx in smoothed_points and end_idx in smoothed_points:
-                    pt1 = smoothed_points[start_idx]
-                    pt2 = smoothed_points[end_idx]
+                startIdx, endIdx = connection
+                if startIdx in smoothedPoints and endIdx in smoothedPoints:
+                    pt1 = smoothedPoints[startIdx]
+                    pt2 = smoothedPoints[endIdx]
                     cv2.line(frame, pt1, pt2, (255, 0, 0), 2)
 
         # Record video
         if self.recording and self.out:
             self.out.write(frame)
-            elapsed = time.time() - self.start_time
-
+            elapsed = time.time() - self.startTime
             if elapsed >= self.recordingLength:
                 self.recording = False
-
                 self.out.release()
                 self.out = None
-                self.countdown_label.config(text="Recording done")
-                # Hide the "Record 5 Seconds" button and show the "Good" and "Bad" buttons
-                self.btn_record.grid_forget()
-                self.btn_good.place(relx=0.4, rely=1, anchor="center")  # Adjust `relx` to center horizontally
-                self.btn_bad.place(relx=0.6, rely=1, anchor="center")
+                self.btnRecord.pack_forget()
+                self.countdownText.pack_forget()
+                self.textFrameText.config(text='Label the hit:')
+                self.textFrameText.pack()
+                self.btnGood.pack(side='left', expand=True, padx=10)
+                self.btnBad.pack(side='left', expand=True, padx=10)
+
+        # Dynamically resize video to fit the video section frame
+        labelWidth = self.videoSection.winfo_width()
+        labelHeight = self.videoSection.winfo_height()
+        if labelWidth > 0 and labelHeight > 0:
+            frame = cv2.resize(frame, (labelWidth, labelHeight), interpolation=cv2.INTER_AREA)
 
         # Convert to ImageTk format and show in UI
-
         img = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
         imgtk = ImageTk.PhotoImage(image=img)
-        self.video_label.imgtk = imgtk
-        self.video_label.configure(image=imgtk)
+        self.videoSection.imgtk = imgtk
+        self.videoSection.configure(image=imgtk)
+        self.gui.after(10, self.videoLoop)
 
-        self.root.after(10, self.video_loop)
 
     def distance(self, p1, p2):
         """
-        Compute Euclidean distance between points p1 and p2
+        Compute the Euclidean distance between points p1 and p2
         """
         return np.linalg.norm(np.array(p1) - np.array(p2))
 
-    def smooth_landmark(self, idx, new_point):
+    def smoothLandmark(self, idx, newPoint):
         """
         Attempts to smooth node updates to be less jittery
         TODO
         - Determine if we want to do this or only use a nodeUpdateThreshold instead
         """
-        new_point = np.array(new_point, dtype=np.float32)
-        if idx not in self.prev_landmarks:
-            self.prev_landmarks[idx] = new_point
+        newPoint = np.array(newPoint, dtype=np.float32)
+        if idx not in self.prevLandmarks:
+            self.prevLandmarks[idx] = newPoint
         else:
+            if self.distance(self.prevLandmarks[idx], newPoint) > self.nodeUpdateThreshold:
+                self.prevLandmarks[idx] = newPoint
 
-            if self.distance(self.prev_landmarks[idx], new_point) > self.nodeUpdateThreshold:
-                self.prev_landmarks[idx] = new_point
+        return tuple(self.prevLandmarks[idx].astype(int))
 
-        return tuple(self.prev_landmarks[idx].astype(int))
+    def saveToFolder(self, label):
 
-    def save_to_folder(self, label):
-
-        if not self.last_filename or not os.path.exists(self.last_filename):
-            tk.messagebox.showerror("Error", "No video to label.")
-
+        if not self.lastFilename or not os.path.exists(self.lastFilename):
+            tk.messagebox.showerror('Error', 'No video to label.')
             return
 
         os.makedirs(label, exist_ok=True)
-        dest = os.path.join(label, os.path.basename(self.last_filename))
-        shutil.move(self.last_filename, dest)
-        print(f"Saved video to: {dest}")
+        dest = os.path.join(label, os.path.basename(self.lastFilename))
+        shutil.move(self.lastFilename, dest)
+        print(f'Saved video to: {dest}')
 
-        # Reset buttons
-        self.btn_good.pack_forget()
-        self.btn_bad.pack_forget()
-        self.last_filename = None
+        # Reset
+        self.btnGood.pack_forget()
+        self.btnBad.pack_forget()
+        self.btnRecord.pack(side='left', expand=True, padx=10)
+        self.textFrameText.config(text='Select an option:')
+        self.lastFilename = None
 
-    def save_good(self):
-        self.save_to_folder("good")
+    def saveGood(self):
+        self.saveToFolder('good')
 
-    def save_bad(self):
-        self.save_to_folder("bad")
+    def saveBad(self):
+        self.saveToFolder('bad')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     PoseRecorderApp()
